04907d306584f5bbb37d804a7ab26c86f69b0fd0
David Miller <dmiller2718@gmail.com>
2026-08-25 16:49:09 -0400

    Update clr.test.check version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/clojure/clr.spec.alpha"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure.clr/test.check "1.1.2"]]
+  :dependencies [[org.clojure.clr/test.check "1.1.3"]]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo/"
                                     :sign-releases  false}]]
   :warn-on-reflection true
